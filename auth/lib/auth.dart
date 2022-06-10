2912f2bc33f1603bac9022a5a98a7cc64fef7535
Ruslan Vafin <vafinruslan-56@bk.ru>
2022-06-11 00:24:03 +0300

    first commit

---
+++ auth/lib/auth.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+import 'package:auth/controllers/app_auth_controller.dart';
+import 'package:conduit/conduit.dart';
+
+class AppService extends ApplicationChannel {
+  late final ManagedContext managedContext;
+
+  @override
+  Future prepare() {
+    final persistentStore = _initDatabase();
+    managedContext = ManagedContext(ManagedDataModel.fromCurrentMirrorSystem(), persistentStore);
+    return super.prepare();
+  }
+
+  @override
+  Controller get entryPoint => Router()..route("token/[:refresh]").link(() => AppAuthController(managedContext));
+  final username = Platform.environment["DB_USERNAME"] ?? "admin";
+  final password = Platform.environment["DB_PASSWORD"] ?? "root";
+  final host = Platform.environment["DB_HOST"] ?? "127.0.0.1";
+  final port = int.parse(Platform.environment["DB_PORT"] ?? "5432");
+  final databaseName = Platform.environment["DB_NAME"] ?? "postgres";
+
+  PostgreSQLPersistentStore _initDatabase() {
+    return PostgreSQLPersistentStore(
+      username,
+      password,
+      host,
+      port,
+      databaseName,
+    );
+  }
+}
