538b2428557eebc5c3866b1d6eeb1b8599d9c9e4
Ruslan Vafin <vafinruslan-56@bk.ru>
2022-06-13 19:56:30 +0300

    add user model and DB

--- auth/lib/controllers/app_auth_controller.dart
+++ auth/lib/controllers/app_auth_controller.dart
@@ -1,4 +1,5 @@
 import 'package:auth/models/rsponse_model.dart';
+import 'package:auth/models/user.dart';
 import 'package:conduit/conduit.dart';
 
 class AppAuthController extends ResourceController {
@@ -6,31 +7,64 @@ class AppAuthController extends ResourceController {
 
   AppAuthController(this.managedContext);
   @Operation.post()
-  Future<Response> signIn() async {
+  Future<Response> signIn(@Bind.body() User user) async {
+    if (user.password == null || user.username == null) {
+      return Response.badRequest(body: ResponseModel(message: "Поля password и username обязательны"));
+    }
+
+    final User fetchedUser = User();
+    //connect to DB
+    //find user
+    //check password
+    //fetch user
+
     return Response.ok(
       ResponseModel(data: {
-        "id": "1",
-        "refreshToken": "refreshToken",
-        "accessToken": "accessToken",
-      }, message: "signIn ok")
+        "id": fetchedUser.id,
+        "refreshToken": fetchedUser.refreshToken,
+        "accessToken": fetchedUser.accessToken,
+      }, message: "Успешная авторизация")
           .toJson(),
     );
   }
 
   @Operation.put()
-  Future<Response> signUp() async {
+  Future<Response> signUp(@Bind.body() User user) async {
+    if (user.password == null || user.username == null || user.email == null) {
+      return Response.badRequest(body: ResponseModel(message: "Поля password, username или email обязательны"));
+    }
+
+    final User fetchedUser = User();
+    //connect to DB
+    //create user
+    //fetch user
+
     return Response.ok(
       ResponseModel(data: {
-        "id": "1",
-        "refreshToken": "refreshToken",
-        "accessToken": "accessToken",
-      }, message: "signUp ok")
+        "id": fetchedUser.id,
+        "refreshToken": fetchedUser.refreshToken,
+        "accessToken": fetchedUser.accessToken,
+      }, message: "Успешная регистрация")
           .toJson(),
     );
   }
 
   @Operation.post("refresh")
-  Future<Response> refreshToken() async {
-    return Response.unauthorized(body: ResponseModel(error: "token is not valid").toJson());
+  Future<Response> refreshToken(@Bind.path("refresh") String refreshToken) async {
+    final User fetchedUser = User();
+
+//connect DB
+//find user
+//check token
+//fethc user
+
+    return Response.ok(
+      ResponseModel(data: {
+        "id": fetchedUser.id,
+        "refreshToken": fetchedUser.refreshToken,
+        "accessToken": fetchedUser.accessToken,
+      }, message: "Успешное обновление токенов")
+          .toJson(),
+    );
   }
 }
