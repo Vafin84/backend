27a7c2c9408e3dc32013f28401d60ddbe7fb7ccb
Ruslan Vafin <vafinruslan-56@bk.ru>
2022-06-23 23:14:56 +0300

    1.11 класс-обертка (wraper) для класса Response

--- auth/lib/controllers/app_auth_controller.dart
+++ auth/lib/controllers/app_auth_controller.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:auth/models/rsponse_model.dart';
 import 'package:auth/models/user.dart';
+import 'package:auth/utils/app_response.dart';
 import 'package:auth/utils/app_utils.dart';
 import 'package:conduit/conduit.dart';
 import 'package:jaguar_jwt/jaguar_jwt.dart';
@@ -29,12 +30,12 @@ class AppAuthController extends ResourceController {
       if (requestHasPassword == findUser.hashPassword) {
         await _updateTokens(findUser.id ?? -1, managedContext);
         final newUser = await managedContext.fetchObjectWithID<User>(findUser.id);
-        return Response.ok(ResponseModel(data: newUser?.backing.contents, message: "Успешная авторизация"));
+        return AppResponse.ok(body: newUser?.backing.contents, message: "Успешная авторизация");
       } else {
         throw QueryException.input("Пароль неверный", []);
       }
-    } on QueryException catch (error) {
-      return Response.serverError(body: ResponseModel(message: error.message));
+    } catch (error) {
+      return AppResponse.serverError(error, message: "Ошибка автроизации");
     }
   }
 
@@ -59,9 +60,9 @@ class AppAuthController extends ResourceController {
         await _updateTokens(id, transaction);
       });
       final userData = await managedContext.fetchObjectWithID<User>(id);
-      return Response.ok(ResponseModel(data: userData?.backing.contents, message: "Успешная регистрация"));
-    } on QueryException catch (error) {
-      return Response.serverError(body: ResponseModel(message: error.message));
+      return AppResponse.ok(body: userData?.backing.contents, message: "Успешная регистрация");
+    } catch (error) {
+      return AppResponse.serverError(error, message: "Ошибка регистрации");
     }
   }
 
@@ -75,10 +76,10 @@ class AppAuthController extends ResourceController {
       } else {
         await _updateTokens(id, managedContext);
         final user = await managedContext.fetchObjectWithID<User>(id);
-        return Response.ok(ResponseModel(data: user?.backing.contents, message: "Успешное обновление токенов"));
+        return AppResponse.ok(body: user?.backing.contents, message: "Успешное обновление токенов");
       }
     } catch (error) {
-      return Response.serverError(body: ResponseModel(message: error.toString()));
+      return AppResponse.serverError(error, message: "Ошибка обновления токенов");
     }
   }
 
---
+++ auth/lib/utils/app_response.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+
+import 'package:auth/models/rsponse_model.dart';
+import 'package:conduit/conduit.dart';
+import 'package:jaguar_jwt/jaguar_jwt.dart';
+
+class AppResponse extends Response {
+  AppResponse.serverError(dynamic error, {String? message})
+      : super.serverError(body: _getResponseModel(error, message));
+
+  static ResponseModel _getResponseModel(error, String? message) {
+    if (error is QueryException) {
+      return ResponseModel(error: error.toString(), message: message ?? error.message);
+    }
+
+    if (error is JwtException) {
+      return ResponseModel(error: error.toString(), message: message ?? error.message);
+    }
+
+    if (error is SocketException) {
+      return ResponseModel(error: error.toString(), message: message ?? error.message);
+    }
+
+    return ResponseModel(error: error.toString(), message: message ?? error.message ?? "Неизвестная ошибка");
+  }
+
+  AppResponse.ok({dynamic body, String? message}) : super.ok(ResponseModel(data: body, message: message));
+}
