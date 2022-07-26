a9ee747a40b6a11e9292e378f559055cbd1137f1
Ruslan Vafin <vafinruslan-56@bk.ru>
2022-07-26 22:54:38 +0300

    уроки 2.4-2.5

--- data/lib/utils/app_env.dart
+++ data/lib/utils/app_env.dart
@@ -5,7 +5,7 @@ abstract class AppEnv {
   static final String secretKey = Platform.environment["SECRET_KEY"] ?? "SECRET_KEY";
   static final String port = Platform.environment["PORT"] ?? "6200";
   static final String dbUsername = Platform.environment["DB_USERNAME"] ?? "admin";
-  static final String dbHost = Platform.environment["DB_HOST"] ?? "localhos";
+  static final String dbHost = Platform.environment["DB_HOST"] ?? "localhost";
   static final String dbPassword = Platform.environment["DB_PASSWORD"] ?? "root";
   static final String dbPort = Platform.environment["DB_PORT"] ?? "6201";
   static final String dbDatabaseName = Platform.environment["DB_NAME"] ?? "postgres";
